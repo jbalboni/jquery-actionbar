6e605e59e7f8ac2b9534e5f020c31ae2f02261c0
jbalboni <jbalboni@gmail.com>
2013-05-28 10:13:54 -0400

    Reorganized actionbar and menu classes

--- jquery.actionbar.js
+++ jquery.actionbar.js
@@ -1,6 +1,7 @@
 (function($){
+    'use strict';
     $.fn.actionbar = function(config) {
-        function ActionBarMenu($ab, menuItems) {
+        function Menu($ab, menuItems) {
             var $menu = $ab.append("<menu/>").find("menu"),
                 $overflowMenu = $ab.append("<menu class='overflow'/>").find("menu.overflow"),
                 width = 0,
@@ -9,11 +10,11 @@
                 overflow = false,
                 maxWidth = getSpace();
             function getSpace () {
-                return $ab.width() - $ab.find('.back-button').width() - $ab.find('.title').width()
-                    - $ab.find('.home-icon').width() - 40;
+                return $ab.width() - $ab.find('.up-button').width() - $ab.find('.title').width()-
+                    $ab.find('.home-icon').width() - 40;
             }
             for (var i = 0; i < menuItems.length; i++) {
-                item = $("<li><a href='#'>" + (menuItems[i].icon ? "<img src='" + menuItems[i].icon + "'/>" : "") +
+                item = $("<li><a href='javascript:void(0);'>" + (menuItems[i].icon ? "<img src='" + menuItems[i].icon + "'/>" : "") +
                     (menuItems[i].icon_only ? "" : menuItems[i].text)  + "</a></li>");
                 $menu.append(item);
                 width = item.width();
@@ -33,43 +34,62 @@
                 $(".actionbar menu.overflow").toggle();
             });
             $(document).on("click", function(evnt) {
-                if (!$(evnt.target).hasClass("overflow-button") && !$(evnt.target).parent().hasClass("overflow-button")) {
+                var $target = $(evnt.target);
+                if (!$target.hasClass("overflow-button") &&
+                    !$target.parent().hasClass("overflow-button")) {
                     $(".actionbar menu.overflow").hide();
                 }
             });
         }
-        function ActionBar(ab) {
+        function ActionBar(ab, title, overflow_icon) {
             //variable creation
             var $abElem = $(ab);
             this.menu = null;
 
             //set up
             $abElem.addClass('actionbar');
+            $abElem.append("<span class=\"title\">" + title + "</span>");
+            $abElem.append("<a href='javascript:void(0);' class='overflow-button'><img src=\"" + overflow_icon + "\"/></a>");
 
             //class methods
             this.createMenu = function (items) {
-                this.menu = new ActionBarMenu($abElem, items);
-            }
+                this.menu = new Menu($abElem, items);
+            };
             this.addHomeButton = function (icon) {
-                $abElem.append("<a href='#' class='home-button'><img class=\"home-icon\" src=\"" + icon + "\"/></a>");
+                $abElem.prepend("<a href='javascript:void(0);' class='home-button'><img class=\"home-icon\" src=\"" + icon + "\"/></a>");
             };
-            this.addTitle = function (title) {
-                $abElem.append("<span class=\"title\">" + title + "</span>");
+            this.addUpButton = function (icon) {
+                $abElem.prepend("<a href='javascript:void(0);' class='up-button'><img class=\"up-icon\" src=\"" + icon + "\"/></a>");
             };
-            this.addOverflow = function (icon) {
-                $abElem.append("<a href='#' class='overflow-button'><img src=\"" + icon + "\"/></a>");
-            }
+            this.setTitle = function (title) {
+                $abElem.find(".title").html(title);
+            };
         }
 
+        //defaults
+        $.extend(config, {
+            show_home_icon: true,
+            home_icon: 'images/ic_launcher.png',
+            overflow_icon: 'images/ic_action_overflow.png',
+            title: 'jQuery Actionbar'
+        });
         return this.each(function(){
-            var ab = new ActionBar(this);
+            var ab = new ActionBar(this, config.overflow_icon);
 
-            ab.addHomeButton(config.home_icon);
-            ab.addTitle(config.title);
-            ab.addOverflow(config.overflow_icon);
-            ab.createMenu(config.menu);
+            if (config.show_up_icon) {
+                ab.addUpButton(config.up_icon);
+            }
+            if (config.show_home_icon) {
+                ab.addHomeButton(config.home_icon);
+            }
 
-            return this;
+            if (config.menu) {
+                ab.createMenu(config.menu);
+            }
+
+            return {
+                setTitle: ab.setTitle
+            };
         });
 
     };
