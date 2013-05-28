405a557d748d3d0b220b4c8a36ba7d072f511434
jbalboni <jbalboni@gmail.com>
2013-05-28 10:46:38 -0400

    Reorganized menu class

--- jquery.actionbar.js
+++ jquery.actionbar.js
@@ -4,35 +4,53 @@
         function Menu($ab, menuItems) {
             var $menu = $ab.append("<menu/>").find("menu"),
                 $overflowMenu = $ab.append("<menu class='overflow'/>").find("menu.overflow"),
-                width = 0,
-                item = null,
-                menuWidth = 0,
-                overflow = false,
-                maxWidth = getSpace();
-            function getSpace () {
-                return $ab.width() - $ab.find('.up-button').width() - $ab.find('.title').width()-
-                    $ab.find('.home-icon').width() - 40;
-            }
-            for (var i = 0; i < menuItems.length; i++) {
-                item = $("<li><a href='javascript:void(0);'>" + (menuItems[i].icon ? "<img src='" + menuItems[i].icon + "'/>" : "") +
-                    (menuItems[i].icon_only ? "" : menuItems[i].text)  + "</a></li>");
-                $menu.append(item);
-                width = item.width();
-                if (overflow === true || menuWidth + width > maxWidth) {
-                    $menu.find("li:last").remove();
-                    $overflowMenu.append(item).find("img").remove();
-                    overflow = true;
-                } else {
-                    menuWidth += width;
-                }
+                getSpace = function() {
+                    return $ab.width() - $ab.find('.up-button').width() - $ab.find('.title').width()-
+                        $ab.find('.home-icon').width() - 70;
+                },
+                buildMenu = function(menuItems) {
+                    var width = 0,
+                        $item = null,
+                        menuWidth = 0,
+                        overflow = false,
+                        maxWidth = getSpace(),
+                        createMenuEl = function(icon, label, show_icon_only) {
+                            var labelHtml = "";
+                            if (typeof icon === "string" && icon !== "") {
+                                labelHtml = "<img src='" + icon + "'/>";
+                            }
+                            if (!show_icon_only) {
+                                labelHtml += label;
+                            }
+                            return $("<li><a href='javascript:void(0);'>" + labelHtml + "</a></li>");
+                        };
 
-            }
+                    $menu.html("");
+
+                    for (var i = 0; i < menuItems.length; i++) {
+                        $item = createMenuEl(menuItems[i].icon, menuItems[i].label, menuItems[i].show_icon_only);
+                        $menu.append($item);
+                        width = $item.width();
+                        if (overflow === true || menuWidth + width > maxWidth) {
+                            $menu.find("li:last").remove();
+                            $overflowMenu.append($item).find("img").remove();
+                            overflow = true;
+                        } else {
+                            menuWidth += width;
+                        }
+                    }
+                };
+
+            //setup
+            buildMenu(menuItems);
             if ($overflowMenu.find("li").size() === 0) {
                 $(".actionbar .overflow-button").hide();
             }
             $(".overflow-button").on("click", function() {
                 $(".actionbar menu.overflow").toggle();
             });
+
+            //events
             $(document).on("click", function(evnt) {
                 var $target = $(evnt.target);
                 if (!$target.hasClass("overflow-button") &&
@@ -40,6 +58,9 @@
                     $(".actionbar menu.overflow").hide();
                 }
             });
+
+            //class methods
+            this.buildMenu = buildMenu;
         }
         function ActionBar(ab, title, overflow_icon) {
             //variable creation
@@ -49,32 +70,35 @@
             //set up
             $abElem.addClass('actionbar');
             $abElem.append("<span class=\"title\">" + title + "</span>");
-            $abElem.append("<a href='javascript:void(0);' class='overflow-button'><img src=\"" + overflow_icon + "\"/></a>");
+            $abElem.append("<a href='javascript:void(0);' class='overflow-button'><img src=\"" + overflow_icon +
+                "\"/></a>");
 
             //class methods
-            this.createMenu = function (items) {
+            this.createMenu = function(items) {
                 this.menu = new Menu($abElem, items);
             };
-            this.addHomeButton = function (icon) {
-                $abElem.prepend("<a href='javascript:void(0);' class='home-button'><img class=\"home-icon\" src=\"" + icon + "\"/></a>");
+            this.addHomeButton = function(icon) {
+                $abElem.prepend("<a href='javascript:void(0);' class='home-button'><img class=\"home-icon\" src=\"" +
+                    icon + "\"/></a>");
             };
-            this.addUpButton = function (icon) {
-                $abElem.prepend("<a href='javascript:void(0);' class='up-button'><img class=\"up-icon\" src=\"" + icon + "\"/></a>");
+            this.addUpButton = function(icon) {
+                $abElem.prepend("<a href='javascript:void(0);' class='up-button'><img class=\"up-icon\" src=\"" +
+                    icon + "\"/></a>");
             };
-            this.setTitle = function (title) {
+            this.setTitle = function(title) {
                 $abElem.find(".title").html(title);
             };
         }
 
         //defaults
-        $.extend(config, {
+        config = $.extend({
             show_home_icon: true,
             home_icon: 'images/ic_launcher.png',
             overflow_icon: 'images/ic_action_overflow.png',
             title: 'jQuery Actionbar'
-        });
+        }, config);
         return this.each(function(){
-            var ab = new ActionBar(this, config.overflow_icon);
+            var ab = new ActionBar(this, config.title, config.overflow_icon);
 
             if (config.show_up_icon) {
                 ab.addUpButton(config.up_icon);
@@ -82,11 +106,9 @@
             if (config.show_home_icon) {
                 ab.addHomeButton(config.home_icon);
             }
-
             if (config.menu) {
                 ab.createMenu(config.menu);
             }
-
             return {
                 setTitle: ab.setTitle
             };
