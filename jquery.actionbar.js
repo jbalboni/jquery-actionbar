947dda5d5f23817d093b5544eda4908585779360
jbalboni <jbalboni@gmail.com>
2013-05-27 23:16:08 -0400

    Reorganized code

--- jquery.actionbar.js
+++ jquery.actionbar.js
@@ -1,41 +1,32 @@
 (function($){
     $.fn.actionbar = function(config) {
-        function getMenuSpace($el) {
-            return $el.width() - $el.find('.back-button').width() - $el.find('.title').width()
-                - $el.find('.home-icon').width() - 40;
-        }
-        return this.each(function(){
-            var $ab = $(this),
-                menu = config.menu,
-                menuEl = $ab.append("<menu/>").find("menu"),
-                overflowMenuEl = $ab.append("<menu class='overflow'/>").find("menu.overflow"),
-                item = null,
+        function ActionBarMenu($ab, menuItems) {
+            var $menu = $ab.append("<menu/>").find("menu"),
+                $overflowMenu = $ab.append("<menu class='overflow'/>").find("menu.overflow"),
                 width = 0,
+                item = null,
                 menuWidth = 0,
-                maxWidth = $ab.width(),
-                overflow = false;
-
-            $ab.addClass('actionbar');
-            $ab.append("<a href='#' class='home-button'><img class=\"home-icon\" src=\"" + config.home_icon + "\"/></a>");
-            $ab.append("<span class=\"title\">" + config.title + "</span>");
-            $ab.append("<a href='#' class='overflow-button'><img src=\"" + config.overflow_icon + "\"/></a>");
-            maxWidth = getMenuSpace($ab);
-
-            for (var i = 0; i < menu.length; i++) {
-                item = $("<li><a href='#'>" + (menu[i].icon ? "<img src='" + menu[i].icon + "'/>" : "") +
-                    (menu[i].icon_only ? "" : menu[i].text)  + "</a></li>");
-                menuEl.append(item);
+                overflow = false,
+                maxWidth = getSpace();
+            function getSpace () {
+                return $ab.width() - $ab.find('.back-button').width() - $ab.find('.title').width()
+                    - $ab.find('.home-icon').width() - 40;
+            }
+            for (var i = 0; i < menuItems.length; i++) {
+                item = $("<li><a href='#'>" + (menuItems[i].icon ? "<img src='" + menuItems[i].icon + "'/>" : "") +
+                    (menuItems[i].icon_only ? "" : menuItems[i].text)  + "</a></li>");
+                $menu.append(item);
                 width = item.width();
                 if (overflow === true || menuWidth + width > maxWidth) {
-                    menuEl.find("li:last").remove();
-                    overflowMenuEl.append(item).find("img").remove();
+                    $menu.find("li:last").remove();
+                    $overflowMenu.append(item).find("img").remove();
                     overflow = true;
                 } else {
                     menuWidth += width;
                 }
 
             }
-            if (overflowMenuEl.find("li").size() === 0) {
+            if ($overflowMenu.find("li").size() === 0) {
                 $(".actionbar .overflow-button").hide();
             }
             $(".overflow-button").on("click", function() {
@@ -46,6 +37,38 @@
                     $(".actionbar menu.overflow").hide();
                 }
             });
+        }
+        function ActionBar(ab) {
+            //variable creation
+            var $abElem = $(ab);
+            this.menu = null;
+
+            //set up
+            $abElem.addClass('actionbar');
+
+            //class methods
+            this.createMenu = function (items) {
+                this.menu = new ActionBarMenu($abElem, items);
+            }
+            this.addHomeButton = function (icon) {
+                $abElem.append("<a href='#' class='home-button'><img class=\"home-icon\" src=\"" + icon + "\"/></a>");
+            };
+            this.addTitle = function (title) {
+                $abElem.append("<span class=\"title\">" + title + "</span>");
+            };
+            this.addOverflow = function (icon) {
+                $abElem.append("<a href='#' class='overflow-button'><img src=\"" + icon + "\"/></a>");
+            }
+        }
+
+        return this.each(function(){
+            var ab = new ActionBar(this);
+
+            ab.addHomeButton(config.home_icon);
+            ab.addTitle(config.title);
+            ab.addOverflow(config.overflow_icon);
+            ab.createMenu(config.menu);
+
             return this;
         });
 
